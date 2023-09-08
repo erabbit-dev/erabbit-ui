599ff95878a63f833fdc78ef9f64e70955110c19
zhousg <zhousg1007@qq.com>
2023-09-08 10:21:19 +0800

    feat(Checkbox): add components Checkbox test case

--- packages/erabbit/src/checkbox/__tests__/index.test.tsx
+++ packages/erabbit/src/checkbox/__tests__/index.test.tsx
@@ -9,3 +9,43 @@ test('should render default Checkbox', () => {
   })
   expect(wrapper.html()).toMatchSnapshot()
 })
+
+test('should use v-model', async () => {
+  const onChange = vitest.fn()
+
+  const wrapper = mount<any>({
+    props: {
+      modelValue: false,
+      indeterminate: false
+    },
+    render() {
+      return (
+        <Checkbox
+          modelValue={this.modelValue}
+          onUpdate:modelValue={(val) => onChange(val)}
+          indeterminate={this.indeterminate}
+        >
+          Checkbox
+        </Checkbox>
+      )
+    }
+  })
+  expect(wrapper.html()).toMatchSnapshot()
+
+  // v-model
+  await wrapper.find('.er-checkbox__origin').trigger('change')
+  expect(onChange).toHaveBeenCalledWith(true)
+
+  await wrapper.setProps({ modelValue: true })
+  expect(wrapper.findAll('.is_checked')).toHaveLength(1)
+
+  await wrapper.setProps({ modelValue: false })
+  expect(wrapper.findAll('.is_checked')).toHaveLength(0)
+
+  // indeterminate
+  await wrapper.setProps({ indeterminate: true })
+  expect(wrapper.findAll('.is_indeterminate')).toHaveLength(1)
+
+  await wrapper.find('.er-checkbox__origin').trigger('change')
+  expect(onChange).toHaveBeenCalledWith(true)
+})
