4c6557345d5e4e5fefb9d6c93d52ce16cda17309
zhousg <zhousg1007@qq.com>
2023-09-13 19:41:58 +0800

    feat(Area): add components Area test case

--- packages/erabbit/src/area/__tests__/index.test.tsx
+++ packages/erabbit/src/area/__tests__/index.test.tsx
@@ -1,11 +1,96 @@
 import { Area } from '..'
 import { mount } from '@vue/test-utils'
+import createFetchMock from 'vitest-fetch-mock'
+import { vi } from 'vitest'
+import { later } from '../../test'
 
-test('should render default Area', () => {
-  const wrapper = mount(Area, {
-    slots: {
-      default: 'Submit'
-    }
+const fetchMocker = createFetchMock(vi)
+
+beforeEach(() => {
+  fetchMocker.enableMocks()
+  fetchMocker.mockIf(/json$/, () => {
+    return `
+    [{
+      "code": "110000",
+      "level": 0,
+      "name": "北京",
+      "areaList": [{
+        "code": "110100",
+        "level": 1,
+        "name": "北京市",
+        "areaList": [{
+          "code": "110101",
+          "level": 2,
+          "name": "东城区"
+        }, {
+          "code": "110102",
+          "level": 2,
+          "name": "西城区"
+        }]
+      }]
+    }]
+    `
   })
+})
+
+test('should render default Area', async () => {
+  const wrapper = mount(Area)
+
   expect(wrapper.html()).toMatchSnapshot()
+})
+
+test('should select Region to display the next region ', async () => {
+  const wrapper = mount(Area)
+
+  await wrapper.find('.er-area__input').trigger('click')
+
+  await later()
+
+  expect(wrapper.html()).toContain('<span class="ellipsis">北京</span>')
+
+  await wrapper.find('.er-area__popper span').trigger('click')
+
+  expect(wrapper.html()).toContain('<span class="ellipsis">北京市</span>')
+
+  await wrapper.find('.er-area__popper span').trigger('click')
+
+  expect(wrapper.html()).toContain('<span class="ellipsis">东城区</span>')
+
+  const outer = document.createElement('div', { class: 'outer' })
+  document.body.appendChild(outer)
+  await outer.click()
+
+  expect(wrapper.findAll('.er-area__popper')).toHaveLength(0)
+})
+
+test('should the change event is triggered after the selection is complete', async () => {
+  const wrapper = mount(Area)
+
+  await wrapper.find('.er-area__input').trigger('click')
+
+  await later()
+
+  expect(wrapper.html()).toContain('<span class="ellipsis">北京</span>')
+
+  await wrapper.find('.er-area__popper span').trigger('click')
+
+  expect(wrapper.html()).toContain('<span class="ellipsis">北京市</span>')
+
+  await wrapper.find('.er-area__popper span').trigger('click')
+
+  expect(wrapper.html()).toContain('<span class="ellipsis">东城区</span>')
+
+  await wrapper.find('.er-area__popper span').trigger('click')
+
+  expect(wrapper.emitted('change')?.[0][0]).toEqual(
+    expect.objectContaining({
+      value: '北京 北京市 东城区'
+    })
+  )
+
+  await wrapper.setProps({
+    value: '北京 北京市 东城区'
+  })
+
+  expect(wrapper.html()).toContain('<span>北京 北京市 东城区</span>')
 })
