5372e6294d52bb33f79d84866305629b51e8a894
zhousg <zhousg1007@qq.com>
2023-09-22 16:48:59 +0800

    release: erabbit v0.2.1

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,5 +3,25 @@
 module.exports = {
   // https://www.npmjs.com/package/@commitlint/config-conventional
   extends: ['@commitlint/config-conventional'],
-  rules: {}
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'release',
+        'style',
+        'test',
+        'improvement'
+      ]
+    ]
+  }
 }
