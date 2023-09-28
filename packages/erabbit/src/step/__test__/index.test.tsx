4f573934e650577e9b2df5fcf7694719f2ca1bcf
shuiruohanyu <shuiruohanyu@foxmail.com>
2023-09-28 16:31:02 +0800

    feat: add new test about step item data control

--- packages/erabbit/src/step/__test__/index.test.tsx
+++ packages/erabbit/src/step/__test__/index.test.tsx
@@ -2,6 +2,7 @@ import { mount } from '@vue/test-utils'
 import { later } from '../../test'
 import { Step, StepItem } from '..'
 import { ref } from 'vue'
+import { expect } from 'vitest'
 
 test('should render default Step', () => {
   const wrapper = mount(
@@ -9,7 +10,7 @@ test('should render default Step', () => {
       <StepItem></StepItem>
       <StepItem></StepItem>
       <StepItem></StepItem>
-    </Step>
+    </Step>,
   )
   expect(wrapper.html()).toMatchSnapshot()
 })
@@ -19,7 +20,7 @@ test('should render  Step with title and desc', () => {
       <StepItem title="开始" desc="2023-09-28 10:23:45"></StepItem>
       <StepItem title="中间" desc="2023-09-28 10:25:46"></StepItem>
       <StepItem title="结束" desc="2023-09-28 10:26:03"></StepItem>
-    </Step>
+    </Step>,
   )
   expect(wrapper.html()).toMatchSnapshot()
 })
@@ -29,7 +30,7 @@ test('should render Step with active first one item', () => {
       <StepItem></StepItem>
       <StepItem></StepItem>
       <StepItem></StepItem>
-    </Step>
+    </Step>,
   )
   expect(wrapper.html()).toMatchSnapshot()
 })
@@ -55,7 +56,7 @@ test('should render  Step with different size', () => {
           </Step>
         </div>
       )
-    }
+    },
   })
   expect(wrapper.html()).toMatchSnapshot()
 })
@@ -65,7 +66,7 @@ test('should render Step with vertical mode', () => {
       <StepItem></StepItem>
       <StepItem></StepItem>
       <StepItem></StepItem>
-    </Step>
+    </Step>,
   )
   expect(wrapper.html()).toMatchSnapshot()
 })
@@ -90,7 +91,7 @@ test('should change acitve item by btn click trigger', async () => {
           </Step>
         </div>
       )
-    }
+    },
   })
   expect(wrapper.html()).toMatchSnapshot()
   wrapper.find('button').trigger('click')
@@ -114,4 +115,66 @@ test('should change acitve item by btn click trigger', async () => {
   wrapper.find('button').trigger('click')
   await later(100)
   expect(stepRef.value.getActiveIndex()).toBe(6)
+})
+
+test('should add or remove a step by button event', async () => {
+  const wrapper = mount({
+    setup() {
+      const list = ref<{
+        title: string
+        desc: string
+      }>([])
+      const addStep = () => {
+        list.value.push({
+          title: `标题${~~(Math.random() * 100)}`,
+          desc: `副标题${~~(Math.random() * 100)}`,
+        })
+      }
+      const removeStep = (index) => {
+        list.value.splice(index, 1)
+      }
+      return () => (
+        <div>
+          <button class="addStep" onClick={addStep}>
+            添加步骤
+          </button>
+          <button
+            class="removeStep"
+            onClick={() => removeStep(list.value.length - 1)}
+          >
+            移除步骤
+          </button>
+          <Step>
+            {list.value.map((item, index) => (
+              <StepItem
+                key={index}
+                title={item.title}
+                desc={item.desc}
+              ></StepItem>
+            ))}
+          </Step>
+        </div>
+      )
+    },
+  })
+  expect(wrapper.html()).toMatchSnapshot()
+  await later()
+  wrapper.find('.addStep').trigger('click')
+  await later()
+  expect(wrapper.findAll('.er-steps-item').length).toBe(1)
+  wrapper.find('.addStep').trigger('click')
+  await later()
+  expect(wrapper.findAll('.er-steps-item').length).toBe(2)
+  wrapper.find('.addStep').trigger('click')
+  await later()
+  expect(wrapper.findAll('.er-steps-item').length).toBe(3)
+  wrapper.find('.removeStep').trigger('click')
+  await later()
+  expect(wrapper.findAll('.er-steps-item').length).toBe(2)
+  wrapper.find('.removeStep').trigger('click')
+  await later()
+  expect(wrapper.findAll('.er-steps-item').length).toBe(1)
+  wrapper.find('.removeStep').trigger('click')
+  await later()
+  expect(wrapper.findAll('.er-steps-item').length).toBe(0)
 })
